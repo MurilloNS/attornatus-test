ee6c7bc8eea22ebd106420d0b2c662ab484242b5
MurilloNS <murillo.murillo2001@gmail.com>
2023-01-03 19:54:42 -0300

    Person Class Created

--- test/test/src/main/java/com/example/attornatus/test/entities/Pessoa.java
+++ test/test/src/main/java/com/example/attornatus/test/entities/Pessoa.java
@@ -1,14 +1,50 @@
 package com.example.attornatus.test.entities;
 
-import java.util.Date;
+import java.time.LocalDate;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
-@Table(name = "tb_persons")
+@Table(name = "tb_pessoas")
 public class Pessoa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
-	private Date nascimento;
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate nascimento;
+
+	public Pessoa() {
+	}
+
+	public Pessoa(String nome, LocalDate nascimento) {
+		this.nome = nome;
+		this.nascimento = nascimento;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(LocalDate nascimento) {
+		this.nascimento = nascimento;
+	}
 }
