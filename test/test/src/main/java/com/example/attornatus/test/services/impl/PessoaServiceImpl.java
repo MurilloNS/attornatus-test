96ae20e503b572033c6e41531df9b4fee891940b
MurilloNS <murillo.murillo2001@gmail.com>
2023-01-03 20:09:20 -0300

    Pessoa Service Implementation Created

---
+++ test/test/src/main/java/com/example/attornatus/test/services/impl/PessoaServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.attornatus.test.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.attornatus.test.services.PessoaService;
+
+@Service
+public class PessoaServiceImpl implements PessoaService {
+
+}
