81369a9c6e5f0aa02bf5509f22bcc3720b4ead05
MurilloNS <murillo.murillo2001@gmail.com>
2023-01-03 20:01:21 -0300

    PessoaRepository Interface Created

---
+++ test/test/src/main/java/com/example/attornatus/test/repositories/PessoaRepository.java
@@ -0,0 +1,8 @@
+package com.example.attornatus.test.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.attornatus.test.entities.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+}
