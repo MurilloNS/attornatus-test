99635d19de9ab41237031163563cd010e9688060
MurilloNS <murillo.murillo2001@gmail.com>
2023-01-04 00:51:24 -0300

    Basic Methods Created

--- test/test/src/main/java/com/example/attornatus/test/services/PessoaService.java
+++ test/test/src/main/java/com/example/attornatus/test/services/PessoaService.java
@@ -1,5 +1,15 @@
 package com.example.attornatus.test.services;
 
-public interface PessoaService {
+import java.util.List;
 
+import com.example.attornatus.test.entities.Pessoa;
+
+public interface PessoaService {
+	public Pessoa savePessoa(Pessoa pessoa);
+
+	public Pessoa updatePessoa(Long id, Pessoa pessoa);
+
+	public Pessoa findByIdPessoa(Long id);
+	
+	public List<Pessoa> listPessoa();
 }
